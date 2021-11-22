e8230db234b295991c1417c3658be68eb5ee8677
briancairl <briancairl@gmail.com>
2021-11-21 17:47:53 -0800

    Adds some more matrix typedefs

--- lib/common/include/matrix.hpp
+++ lib/common/include/matrix.hpp
@@ -16,4 +16,15 @@ using Vec3f = Eigen::Matrix<float, 3, 1>;
 using Mat2f = Eigen::Matrix<float, 2, 2>;
 using Mat3f = Eigen::Matrix<float, 3, 3>;
 
+using VecXf = Eigen::Matrix<float, Eigen::Dynamic, 1>;
+using MatXf = Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic>;
+
+using Vec2i = Eigen::Matrix<int, 2, 1>;
+using Vec3i = Eigen::Matrix<int, 3, 1>;
+using Mat2i = Eigen::Matrix<int, 2, 2>;
+using Mat3i = Eigen::Matrix<int, 3, 3>;
+
+using VecXi = Eigen::Matrix<int, Eigen::Dynamic, 1>;
+using MatXi = Eigen::Matrix<int, Eigen::Dynamic, Eigen::Dynamic>;
+
 }  // namespace tyl
