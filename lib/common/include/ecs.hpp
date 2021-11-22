2cbabe7d0a278bb622e43cda7abb065d17460736
briancairl <briancairl@gmail.com>
2021-11-21 17:46:51 -0800

    Adds ECS wrapper module for EnTT types

---
+++ lib/common/include/ecs.hpp
@@ -0,0 +1,17 @@
+/**
+ * @copyright 2021-present Brian Cairl
+ *
+ * @file entt.hpp
+ */
+#pragma once
+
+// Eigen
+#include <entt/entt.hpp>
+
+namespace tyl
+{
+
+using Entity = entt::entity;
+using ECSRegistry = entt::registry;
+
+}  // namespace tyl
