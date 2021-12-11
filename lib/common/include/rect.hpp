23c3a0eb3a3ddcb5a638ae205b74f195a1a195c9
briancairl <briancairl@gmail.com>
2021-12-11 14:24:27 -0800

    Updates `Rect` utility class
    - Adds `scale_from_center` and `center` methods
    - Adds dox

--- lib/common/include/rect.hpp
+++ lib/common/include/rect.hpp
@@ -5,9 +5,6 @@
  */
 #pragma once
 
-// C++ Standard Library
-#include <initializer_list>
-
 // Eigen
 #include <Eigen/Dense>
 
@@ -21,9 +18,22 @@ template <typename T> class Rect
 {
 public:
   using corner_type = Eigen::Matrix<T, 2, 1>;
+
   using extents_type = Eigen::Matrix<T, 2, 1>;
 
+  /**
+   * @brief Creates a Rect from corner locations
+   */
   inline static Rect corners(const corner_type& min, const corner_type& max) { return Rect{min, max - min}; }
+
+  /**
+   * @brief Returns a rectangle, scaled from the center point of the original rectangle
+   */
+  inline static Rect scaled_from_center(const Rect<T>& rect, const T scaling)
+  {
+    const corner_type c{rect.center()};
+    return Rect::corners(scaling * (rect.min() - c) + c, scaling * (rect.max() - c) + c);
+  }
 
   Rect(const Rect&) = default;
 
@@ -31,39 +41,92 @@ public:
 
   Rect() : Rect{corner_type::Zero(), extents_type::Zero()} {}
 
+  /**
+   * @brief Returns lower left corner of the rectangle
+   */
   constexpr const corner_type& min() const { return corner_; }
 
+  /**
+   * @brief Returns upper right corner of the rectangle
+   */
   constexpr const corner_type max() const { return corner_ + extents_; }
 
-  constexpr const corner_type upper_left() const { return corner_type{corner_.x(), corner_.y() + extents_.y()}; }
+  /**
+   * @brief Returns upper left corner of the rectangle
+   */
+  constexpr corner_type upper_left() const { return corner_type{corner_.x(), corner_.y() + extents_.y()}; }
 
-  constexpr const corner_type lower_right() const { return corner_type{corner_.x() + extents_.x(), corner_.y()}; }
+  /**
+   * @brief Returns lower right corner of the rectangle
+   */
+  constexpr corner_type lower_right() const { return corner_type{corner_.x() + extents_.x(), corner_.y()}; }
 
+  /**
+   * @brief Returns center point of the the rectangle
+   */
+  inline corner_type center() const
+  {
+    if constexpr (std::is_floating_point<T>())
+    {
+      static constexpr T MUL = 0.5;
+      return corner_ + MUL * extents_;
+    }
+    else
+    {
+      static constexpr T DIV = 2;
+      return corner_ + extents_ / DIV;
+    }
+  }
+
+  /**
+   * @brief Returns the positive extents of the rectangle along each axis
+   */
   constexpr const extents_type& extents() const { return extents_; }
 
+  /**
+   * @brief Checks if a point is within the rectangular region
+   */
   inline bool within(const corner_type& query) const { return within_impl(query, corner_, corner_ + extents_); }
 
+  /**
+   * @brief Checks if rectngle intersects (overlaps) with another rectangular region
+   */
   inline bool intersects(const Rect& other) const
   {
     return intersects_impl(*this, other) or intersects_impl(other, *this);
   }
 
+  /**
+   * @brief Sets the lower left corner location of the rectangle to \c corner
+   */
   inline void snap(const corner_type& corner) { corner_ = corner; }
 
+  /**
+   * @brief Offsets rectangle by \c offset
+   */
   inline Rect& operator+=(const extents_type& offset)
   {
     corner_ += offset;
     return *this;
   }
 
+  /**
+   * @brief Offsets rectangle by negative \c offset
+   */
   inline Rect& operator-=(const extents_type& offset)
   {
     corner_ -= offset;
     return *this;
   }
 
+  /**
+   * @brief Returns a rectangle offset by \c offset
+   */
   inline Rect operator+(const extents_type& offset) const { return Rect{corner_ + offset, extents_}; }
 
+  /**
+   * @brief Returns a rectangle offset by negative \c offset
+   */
   inline Rect operator-(const extents_type& offset) const { return Rect{corner_ - offset, extents_}; }
 
 private:
@@ -86,7 +149,6 @@ private:
   corner_type corner_;
   extents_type extents_;
 };
-
 
 using Rect2i = Rect<int>;
 
