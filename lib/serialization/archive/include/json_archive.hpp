2201b113d9f591ae154cee7b0ced851e55849e46
Brian Cairl <briancairl@gmail.com>
2022-08-25 11:48:00 -0400

    Adds meta includes for streams/archives

---
+++ lib/serialization/archive/include/json_archive.hpp
@@ -0,0 +1,10 @@
+/**
+ * @copyright 2022-present Brian Cairl
+ *
+ * @file json_archive.hpp
+ */
+#pragma once
+
+// Tyl
+#include <tyl/serialization/json_iarchive.hpp>
+#include <tyl/serialization/json_oarchive.hpp>
---
+++ lib/serialization/stream/include/file_stream.hpp
@@ -0,0 +1,10 @@
+/**
+ * @copyright 2022-present Brian Cairl
+ *
+ * @file file_stream.hpp
+ */
+#pragma once
+
+// Tyl
+#include <tyl/serialization/file_istream.hpp>
+#include <tyl/serialization/file_ostream.hpp>
