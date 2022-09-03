92c267e5f698316f2d3e8d47ac6e24d74c179d3a
Brian Cairl <briancairl@gmail.com>
2022-09-02 20:56:38 -0400

    Adds sprite animation/render systems
    - Moves lib/sprite

---
+++ systems/graphics/include/sprite_rendering.hpp
@@ -0,0 +1,82 @@
+/**
+ * @copyright 2022-present Brian Cairl
+ *
+ * @file sprite_rendering.hpp
+ */
+#pragma once
+
+// Tyl
+#include <tyl/ecs/ecs.hpp>
+#include <tyl/ecs/reference.hpp>
+#include <tyl/graphics/device/fwd.hpp>
+
+namespace tyl::graphics
+{
+
+struct RenderTarget2D;
+
+using AtlasTextureReference = ecs::ref<device::Texture, device::TextureHandle>;
+
+struct SpriteRendererOptions
+{
+  /// Total number of sprites that can be rendered
+  std::size_t capacity = 100UL;
+
+  /// Texture unit to bind atlas texture to
+  int atlas_texture_unit = 0;
+};
+
+struct SpriteRendererState
+{
+  /// Total number of sprites currently being rendered on update
+  std::size_t size;
+};
+
+/**
+ * @brief Creates a system which renders static/animated 2D sprites
+ */
+ecs::entity
+create_sprite_renderer(ecs::registry& reg, const AtlasTextureReference& texture, const SpriteRendererOptions& options);
+
+/**
+ * @brief Attaches components for a system which renders static/animated 2D sprites
+ */
+void attach_sprite_renderer(ecs::registry& reg, const ecs::entity e, const SpriteRendererOptions& options);
+
+/**
+ * @brief Updates 2D sprite renderers
+ */
+void update_sprite_renderers(ecs::registry& reg, const RenderTarget2D& target);
+
+}  // namespace tyl::graphics
+
+// Tyl
+#include <tyl/serial.hpp>
+
+namespace tyl::serialization
+{
+
+/**
+ * @brief Archive-generic <code>::tyl::graphics::SpriteRendererOptions</code> serialize implementation
+ */
+template <typename ArchiveT> struct serialize<ArchiveT, ::tyl::graphics::SpriteRendererOptions>
+{
+  void operator()(ArchiveT& ar, ::tyl::graphics::SpriteRendererOptions& options)
+  {
+    ar& named{"capacity", options.capacity};
+    ar& named{"atlas_texture_unit", options.atlas_texture_unit};
+  }
+};
+
+/**
+ * @brief Archive-generic <code>::tyl::graphics::SpriteRendererState</code> serialize implementation
+ */
+template <typename ArchiveT> struct serialize<ArchiveT, ::tyl::graphics::SpriteRendererState>
+{
+  void operator()(ArchiveT& ar, ::tyl::graphics::SpriteRendererState& properties)
+  {
+    ar& named{"size", properties.size};
+  }
+};
+
+}  // namespace tyl::serialization
---
+++ systems/graphics/test/sprite_renderer/sprite_renderer.cpp
@@ -0,0 +1,130 @@
+/**
+ * @copyright 2022-present Brian Cairl
+ *
+ * @file sprite_renderer.cpp
+ */
+
+// C++ Standard Library
+#include <cstdio>
+#include <exception>
+
+// GLAD
+#include <glad/glad.h>
+
+// Include glfw3.h after our OpenGL definitions
+#include <GLFW/glfw3.h>
+
+// Tyl
+#include <tyl/ecs/ecs.hpp>
+#include <tyl/graphics/common.hpp>
+#include <tyl/graphics/device/debug.hpp>
+#include <tyl/graphics/device/texture.hpp>
+#include <tyl/graphics/host/image.hpp>
+#include <tyl/graphics/render_target.hpp>
+#include <tyl/graphics/sprite.hpp>
+#include <tyl/graphics/sprite_animation.hpp>
+#include <tyl/graphics/sprite_rendering.hpp>
+#include <tyl/math/size.hpp>
+#include <tyl/math/vec.hpp>
+
+static void glfw_error_callback(int error, const char* description)
+{
+  std::fprintf(stderr, "%d : %s\n", error, description);
+}
+
+
+int main(int argc, char** argv)
+{
+  glfwSetErrorCallback(glfw_error_callback);
+  if (!glfwInit())
+  {
+    std::terminate();
+  }
+
+  int x_size = 500;
+  int y_size = 500;
+
+  // Create window with graphics context
+  GLFWwindow* window = glfwCreateWindow(x_size, y_size, "sprite-renderer-test", NULL, NULL);
+
+  if (window == NULL)
+  {
+    std::terminate();
+  }
+
+  glfwMakeContextCurrent(window);
+  gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+  glfwSwapInterval(1);  // Enable vsync
+  glfwSetInputMode(window, GLFW_STICKY_KEYS, GLFW_TRUE);
+
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+  tyl::graphics::device::enable_debug_logs();
+  tyl::graphics::device::enable_error_logs();
+
+  tyl::ecs::registry reg;
+
+  // Upload texture data
+  const auto texture_guid = reg.create();
+  {
+    const auto texture_host = tyl::graphics::host::load("systems/graphics/test/sprite_renderer/spritesheet.png");
+
+    reg.emplace<tyl::Vec2i>(texture_guid, texture_host.height(), texture_host.width());
+    const auto& texture = reg.emplace<tyl::graphics::device::Texture>(texture_guid, texture_host);
+
+    tyl::graphics::create_sprite_renderer(reg, {texture_guid, texture}, {.capacity = 1000, .atlas_texture_unit = 0});
+  }
+
+  // Sprite 1
+  {
+    const auto e = tyl::graphics::create_sprite(
+      reg,
+      {0.f, 0.f},
+      {1.f, 1.f},
+      {.atlas_texture_size_px = reg.get<tyl::Vec2i>(texture_guid),
+       .subdivisions = {4, 1},
+       .inner_padding_px = {0, 0},
+       .area_px = {{0, 0}, {128, 48}},
+       .transpose = false});
+
+    tyl::graphics::attach_sprite_animation_repeating(reg, e, {.progress_per_second = 1.f});
+    tyl::graphics::enable_rendering(reg, e);
+  }
+
+  // Sprite 2
+  {
+    const auto e = tyl::graphics::create_sprite(
+      reg,
+      {-1.f, -1.f},
+      {1.f, 1.f},
+      {.atlas_texture_size_px = reg.get<tyl::Vec2i>(texture_guid),
+       .subdivisions = {4, 1},
+       .inner_padding_px = {0, 0},
+       .area_px = {{0, 0}, {128, 48}},
+       .transpose = false});
+
+    tyl::graphics::attach_sprite_animation_one_shot(reg, e, {.progress_per_second = 1.f});
+    tyl::graphics::enable_rendering(reg, e);
+  }
+
+  tyl::graphics::RenderTarget2D render_target;
+  render_target.view_projection.setIdentity();
+
+  while (!glfwWindowShouldClose(window))
+  {
+    glfwPollEvents();
+
+    glClearColor(0, 0, 0, 0);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    tyl::graphics::update_sprite_renderers(reg, render_target);
+    tyl::graphics::update_sprite_animations(reg, 0.01f);
+
+    glfwGetFramebufferSize(window, &x_size, &y_size);
+    glViewport(0, 0, x_size, y_size);
+    glfwSwapBuffers(window);
+  }
+
+  return 0;
+}
