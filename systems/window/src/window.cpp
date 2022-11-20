b2ac681fd4cb8cb563c545eddb601b739d71c392
Brian Cairl <briancairl@gmail.com>
2022-11-20 16:21:57 -0500

    Adds window/loop system

---
+++ systems/window/src/window.cpp
@@ -0,0 +1,194 @@
+/**
+ * @copyright 2022-present Brian Cairl
+ *
+ * @file window.cpp
+ */
+
+// C++ Standard Library
+#include <cstdio>
+#include <exception>
+
+// GLAD
+#include <glad/glad.h>
+
+// Include glfw3.h after our OpenGL definitions
+#include <GLFW/glfw3.h>
+
+// Tyl
+#include <tyl/window/window.hpp>
+
+namespace tyl
+{
+namespace
+{
+namespace glfw
+{
+void message_callback(int error, const char* description)
+{
+  std::fprintf(stderr, "[GLFW][%.6d] : %s\n", error, description);
+}
+
+int init()
+{
+  glfwSetErrorCallback(message_callback);
+  if (!glfwInit())
+  {
+    std::terminate();
+  }
+  message_callback(0, "initialized");
+  return 0;
+}
+
+template <std::size_t Bit, typename BitFieldT, typename BlockT, std::size_t BlockCount, typename KeyCodeT>
+void read_key_states(
+  GLFWwindow* window,
+  bitfield<BitFieldT, BlockT, BlockCount>& pressed_bitfield,
+  bitfield<BitFieldT, BlockT, BlockCount>& released_bitfield,
+  bitfield<BitFieldT, BlockT, BlockCount>& repeat_bitfield,
+  const KeyCodeT key_code)
+{
+  const auto key_state = glfwGetKey(window, key_code);
+  pressed_bitfield.template set_to<Bit>(key_state == GLFW_PRESS);
+  released_bitfield.template set_to<Bit>(key_state == GLFW_RELEASE);
+  repeat_bitfield.template set_to<Bit>(key_state == GLFW_REPEAT);
+}
+
+template <
+  typename BitFieldT,
+  typename BlockT,
+  std::size_t BlockCount,
+  typename KeyCodeTupleT,
+  std::size_t... BitOffsets>
+void read_many_key_states_impl(
+  GLFWwindow* window,
+  bitfield<BitFieldT, BlockT, BlockCount>& pressed_bitfield,
+  bitfield<BitFieldT, BlockT, BlockCount>& released_bitfield,
+  bitfield<BitFieldT, BlockT, BlockCount>& repeat_bitfield,
+  KeyCodeTupleT&& key_codes,
+  std::integer_sequence<std::size_t, BitOffsets...> bit_offsets)
+{
+  [[maybe_unused]] const auto _ =
+    ((read_key_states<BitOffsets>(
+        window,
+        pressed_bitfield,
+        released_bitfield,
+        repeat_bitfield,
+        std::get<BitOffsets>(std::forward<KeyCodeTupleT>(key_codes))),
+      0) +
+     ...);
+}
+
+template <typename BitFieldT, typename BlockT, std::size_t BlockCount, typename... KeyCodeTs>
+void read_many_key_states(
+  GLFWwindow* window,
+  bitfield<BitFieldT, BlockT, BlockCount>& pressed_bitfield,
+  bitfield<BitFieldT, BlockT, BlockCount>& released_bitfield,
+  bitfield<BitFieldT, BlockT, BlockCount>& repeat_bitfield,
+  KeyCodeTs... key_codes)
+{
+  read_many_key_states_impl(
+    window,
+    pressed_bitfield,
+    released_bitfield,
+    repeat_bitfield,
+    std::forward_as_tuple(key_codes...),
+    std::make_integer_sequence<std::size_t, sizeof...(KeyCodeTs)>{});
+}
+
+}  // namespace glfw
+}  // namespace
+
+Window::Window(Options&& options) : window_state_{.size = options.size, .cursor_position = {0.0, 0.0}}
+{
+  // Initialize GLFW once
+  [[maybe_unused]] static int _ = glfw::init();
+
+  // Create window with graphics context
+  GLFWwindow* window = glfwCreateWindow(options.size.width, options.size.height, options.title, NULL, NULL);
+
+  if (window == NULL)
+  {
+    std::terminate();
+  }
+
+  glfwMakeContextCurrent(window);
+  gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+  glfwSwapInterval(options.flags.enable_vsync ? 1 : 0);
+  glfwSetInputMode(window, GLFW_STICKY_KEYS, options.flags.enable_sticky_keys ? GLFW_TRUE : GLFW_FALSE);
+
+  window_handle_ = reinterpret_cast<void*>(window);
+}
+
+Window::~Window()
+{
+  auto* const window = reinterpret_cast<GLFWwindow*>(window_handle_);
+  glfwDestroyWindow(window);
+}
+
+bool Window::window_state_update()
+{
+  auto* const window = reinterpret_cast<GLFWwindow*>(window_handle_);
+
+  // Handle close out
+  if (glfwWindowShouldClose(window))
+  {
+    return false;
+  }
+
+  // Get viewport size
+  glfwGetFramebufferSize(window, &window_state_.size.width, &window_state_.size.height);
+
+  // Set active GL frame buffer size
+  glViewport(0, 0, window_state_.size.width, window_state_.size.height);
+
+  // Swap buffers
+  glfwSwapBuffers(window);
+
+  // Poll window events
+  glfwPollEvents();
+
+  // Get cursor coordinates in pixel space
+  glfwGetCursorPos(window, &window_state_.cursor_position.y, &window_state_.cursor_position.x);
+
+  // Remaps mouse coordinates to [-1, 1]
+  {
+    const double h_w = 0.5 * window_state_.size.width;
+    const double h_h = 0.5 * window_state_.size.height;
+    window_state_.aspect_ratio = h_w / h_h;
+    window_state_.cursor_position_normalized.x = (window_state_.cursor_position.x - h_h) / h_h;
+    window_state_.cursor_position_normalized.y =
+      (window_state_.cursor_position.y - h_w) / h_w * window_state_.aspect_ratio;
+  }
+
+  // Poll for key states
+  glfw::read_many_key_states(
+    window,
+    window_state_.key_pressed_flags,
+    window_state_.key_pressed_flags,
+    window_state_.key_released_flags,
+    GLFW_KEY_W,
+    GLFW_KEY_A,
+    GLFW_KEY_S,
+    GLFW_KEY_D,
+    GLFW_KEY_Q,
+    GLFW_KEY_E,
+    GLFW_KEY_Z,
+    GLFW_KEY_C,
+    GLFW_KEY_0,
+    GLFW_KEY_1,
+    GLFW_KEY_2,
+    GLFW_KEY_3,
+    GLFW_KEY_4,
+    GLFW_KEY_5,
+    GLFW_KEY_6,
+    GLFW_KEY_7,
+    GLFW_KEY_8,
+    GLFW_KEY_9);
+
+  // Clear frame buffer
+  glClearColor(0, 0, 0, 0);
+  glClear(GL_COLOR_BUFFER_BIT);
+  return true;
+};
+
+}  // namespace tyl
