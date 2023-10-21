1a3beeea2a2cf46b91a7d14d3a6424f616a87b46
Brian Cairl <briancairl@gmail.com>
2023-10-21 14:07:40 -0400

    Adds atlas texture navigation

--- engine/widget/src/widget_tileset_creator.cpp
+++ engine/widget/src/widget_tileset_creator.cpp
@@ -38,12 +38,39 @@ namespace tyl::engine
 using Image = graphics::host::Image;
 using Texture = graphics::device::Texture;
 
+struct ImTransform
+{
+  ImVec2 offset = {0, 0};
+  float scaling = 1;
+};
+
+ImTransform inverse(const ImTransform& transform)
+{
+  const float inv_scaling = 1.f / transform.scaling;
+  return ImTransform{.offset = -transform.offset * transform.scaling, .scaling = inv_scaling};
+}
+
+ImVec2 operator*(const ImTransform& transform, const ImVec2 pt) { return pt * transform.scaling + transform.offset; }
+
+ImTransform operator*(const ImTransform& lhs, const ImTransform& rhs)
+{
+  return ImTransform{
+    .offset = {lhs.scaling * rhs.offset.x + lhs.offset.x, lhs.scaling * rhs.offset.y + lhs.offset.y},
+    .scaling = (lhs.scaling * rhs.scaling)};
+}
+
 struct TileSetSelection
 {
   Vec2i dims = {10, 10};
   Vec2f offset = {0.f, 0.f};
   Vec2f cell_size = {25.f, 25.f};
   dynamic_bitset<std::uint64_t> selected = dynamic_bitset<std::uint64_t>{10 * 10};
+};
+
+struct TileSetAtlasTextureEdittingState
+{
+  ImTransform texture_transform;
+  std::optional<ImTransform> texture_transform_on_nav_start = std::nullopt;
 };
 
 struct TileSet
@@ -126,7 +153,7 @@ public:
 
     // Handle drag/drop
     const auto loaded_texture_ids_or_error = drag_and_drop_images_.update(
-      registry, shared, resources, [is_hovered = texture_atlas_is_hovered] { return is_hovered; });
+      registry, shared, resources, [is_hovered = texture_atlas_is_hovered_] { return is_hovered; });
 
     // Create reference to first loaded texture
     if (loaded_texture_ids_or_error.has_value() and !loaded_texture_ids_or_error->empty())
@@ -135,32 +162,79 @@ public:
     }
   }
 
+  static void TileSetAtlasTextureNav(const ImVec2 mouse_pos, TileSetAtlasTextureEdittingState& state)
+  {
+    auto& io = ImGui::GetIO();
+    if (!io.KeyCtrl)
+    {
+      state.texture_transform_on_nav_start.reset();
+      return;
+    }
+
+    // Zooming
+    if (const float delta = io.MouseWheel; delta != 0)
+    {
+      const float scale_delta = 1e-1f * delta;
+      const ImTransform mouse_transform{mouse_pos, 1.f};
+      const ImTransform mouse_relative_to_texture = inverse(mouse_transform) * state.texture_transform;
+      state.texture_transform =
+        mouse_transform * ImTransform{.scaling = (1.f + scale_delta)} * mouse_relative_to_texture;
+    }
+
+    // Moving with mouse
+    if (!ImGui::IsMouseDown(ImGuiPopupFlags_MouseButtonLeft))
+    {
+      state.texture_transform_on_nav_start.reset();
+    }
+    else if (state.texture_transform_on_nav_start)
+    {
+      const auto delta = ImGui::GetMouseDragDelta(ImGuiPopupFlags_MouseButtonLeft);
+      state.texture_transform.offset = state.texture_transform_on_nav_start->offset + delta;
+    }
+    else
+    {
+      state.texture_transform_on_nav_start = state.texture_transform;
+    }
+  }
+
   void TileSetAtlasTexture(Registry& registry, WidgetSharedState& shared, const WidgetResources& resources)
   {
+    const auto draw_origin = ImGui::GetCursorScreenPos();
+
     static constexpr bool kChildShowBoarders = false;
-    static constexpr auto kChildFlags = ImGuiWindowFlags_HorizontalScrollbar;
+    static constexpr auto kChildFlags = ImGuiWindowFlags_HorizontalScrollbar | ImGuiWindowFlags_NoScrollWithMouse;
     ImGui::BeginChild("#TileSetAtlasTexture", ImVec2{0, 0}, kChildShowBoarders, kChildFlags);
     if (active_tile_set_id_)
     {
+      auto* drawlist = ImGui::GetWindowDrawList();
       if (registry.any_of<Reference<Texture>>(*active_tile_set_id_))
       {
-        const auto& texture_ref = registry.get<Reference<Texture>>(*active_tile_set_id_);
+        auto [texture_ref, editting_state] =
+          registry.get<Reference<Texture>, TileSetAtlasTextureEdittingState>(*active_tile_set_id_);
+
         const auto& texture = resolve(registry, texture_ref);
-        ImGui::Image(
+        const ImVec2 texture_size{
+          static_cast<float>(texture.shape().height), static_cast<float>(texture.shape().width)};
+
+        if (ImGui::IsWindowHovered())
+        {
+          TileSetAtlasTextureNav(ImGui::GetMousePos() - draw_origin, editting_state);
+        }
+
+        drawlist->AddImage(
           reinterpret_cast<void*>(texture.get_id()),
-          ImVec2(texture.shape().height, texture.shape().width),
+          draw_origin + editting_state.texture_transform * ImVec2{0, 0},
+          draw_origin + editting_state.texture_transform * texture_size,
           ImVec2(0, 0),
-          ImVec2(1, 1),
-          ImVec4(1, 1, 1, 1),
-          ImVec4(0, 0, 0, 0));
+          ImVec2(1, 1));
       }
       else
       {
-        ImGui::TextColored(ImVec4{1, 0, 0, 1}, "drag tileset texture here");
+        drawlist->AddText(draw_origin + ImVec2{50, 50}, ImColor{1.0f, 0.5f, 0.5f, 1.0f}, "DROP TEXTURE HERE");
       }
     }
     ImGui::EndChild();
-    texture_atlas_is_hovered = ImGui::IsItemHovered();
+    texture_atlas_is_hovered_ = ImGui::IsItemHovered();
     TileSetAtlasTextureDragAndDropExternalSink(registry, shared, resources);
     TileSetAtlasTextureDragAndDropInternalSink(registry);
   }
@@ -221,6 +295,7 @@ public:
         const auto id = registry.create();
         registry.emplace<std::string>(id, kTileSetNameBuffer);
         registry.emplace<TileSet>(id);
+        registry.emplace<TileSetAtlasTextureEdittingState>(id);
         registry.emplace<std::vector<TileSetSelection>>(id, std::vector<TileSetSelection>{});
         active_tile_set_id_ = id;
       }
@@ -231,12 +306,13 @@ public:
     ImGui::EndPopup();
   }
 
-  constexpr bool LockWindowMovement() const { return texture_atlas_is_hovered; }
+  constexpr bool LockWindowMovement() const { return texture_atlas_is_hovered_; }
 
 private:
-  bool texture_atlas_is_hovered = false;
+  bool texture_atlas_is_hovered_ = false;
   bool tile_set_naming_pop_up_open_ = false;
   std::optional<EntityID> active_tile_set_id_;
+  std::optional<TileSetSelection> editting_tile_set_selection_;
   DragAndDropImages drag_and_drop_images_;
 };
 
