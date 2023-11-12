0cea25b251fc81d6da8b52d0beff8051bbcc8b56
Brian Cairl <briancairl@gmail.com>
2023-11-12 11:44:36 -0500

    Split `TileMap` in into `TileMapSection`

--- engine/graphics/include/tile_map.hpp
+++ engine/graphics/include/tile_map.hpp
@@ -9,6 +9,7 @@
 #include <vector>
 
 // Tyl
+#include <tyl/engine/ecs.hpp>
 #include <tyl/engine/math.hpp>
 #include <tyl/serialization/object.hpp>
 #include <tyl/serialization/std/vector.hpp>
@@ -17,6 +18,12 @@ namespace tyl::engine
 {
 
 struct TileMap
+{
+  /// Represents an MxN grid of TileMapSections
+  Mat<Entity, Eigen::Dynamic, Eigen::Dynamic> sections;
+};
+
+struct TileMapSection
 {
   /// Represents an MxN grid of tiles selected from a TileSet. The size of each tile is dependent on the TileSet it
   /// comes from
@@ -30,7 +37,12 @@ namespace tyl::serialization
 
 template <typename ArchiveT> struct serialize<ArchiveT, engine::TileMap>
 {
-  void operator()(ArchiveT& ar, engine::TileMap& tile_set) { ar& named{"tile_indices", tile_set.tile_indices}; }
+  void operator()(ArchiveT& ar, engine::TileMap& tile_set) { ar& named{"sections", tile_set.sections}; }
+};
+
+template <typename ArchiveT> struct serialize<ArchiveT, engine::TileMapSection>
+{
+  void operator()(ArchiveT& ar, engine::TileMapSection& tile_set) { ar& named{"tile_indices", tile_set.tile_indices}; }
 };
 
 }  // namespace tyl::serialization
